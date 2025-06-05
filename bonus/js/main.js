2e3f0acdf0b4db4553c1f96e77ed3d0b70ffc72b
simone-sampino <s.samp98@gmail.com>
2025-06-05 19:49:56 +0200

    Refactor lamp toggle functionality to include conditional logic for image and button text updates

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -10,9 +10,16 @@ const btnEl = document.getElementById("on-off");
 
 btnEl.addEventListener("click", function () {
   //   console.log("clicked");
-  imgEl.src = "../img/yellow_lamp.png";
-  imgEl.alt = "light on";
-  //   console.log(imgEl);
-  btnEl.innerHTML = "Spegni";
-  //   console.log(btnEl);
+  if (imgEl.src.includes("white_lamp.png")) {
+    imgEl.src = "../img/yellow_lamp.png";
+    imgEl.alt = "light on";
+    // console.log(imgEl);
+    btnEl.innerHTML = "Spegni";
+    // console.log(btnEl);
+  } else {
+    // procedimento inverso
+    imgEl.src = "../img/white_lamp.png";
+    imgEl.alt = "light off";
+    btnEl.innerHTML = "Accendi";
+  }
 });
