9d206fbf25f2b1ed16312066f6208ab06b8c3a4d
simone-sampino <s.samp98@gmail.com>
2025-06-05 18:42:47 +0200

    Add button ID for lamp toggle functionality

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 const imgEl = document.querySelector("img");
 
-imgEl.src = "../img/yellow_lamp.png";
-imgEl.alt = "light on";
+// imgEl.src = "../img/yellow_lamp.png";
+// imgEl.alt = "light on";
 // console.log(imgEl);
+
+const btnEl = document.getElementById("on-off");
+// console.log(btnEl);
+
+btnEl.addEventListener("click", function () {
+  //   console.log("clicked");
+  imgEl.src = "../img/yellow_lamp.png";
+  imgEl.alt = "light on";
+  //   console.log(imgEl);
+});
